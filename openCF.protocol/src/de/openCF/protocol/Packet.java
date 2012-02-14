3195f6cbe208de738824a1b1fef50e49c73945ce
kristian.wessels@gmail.com <kristian.wessels@gmail.com@d1be82d6-222f-3150-1200-e2c27961b611>
2012-02-14 13:04:44 +0000

    Initial import.

---
+++ openCF.protocol/src/de/openCF/protocol/Packet.java
@@ -0,0 +1,76 @@
+package de.openCF.protocol;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class Packet {
+
+	private int					dataLengt	= 0;
+	private byte[]				rawData		= null;
+	private Map<String, Object>	data		= null;
+
+	public Packet() {
+		super();
+	}
+
+	public int getDataLengt() {
+		return dataLengt;
+	}
+
+	public void setDataLengt(int dataLengt) {
+		this.dataLengt = dataLengt;
+	}
+
+	public byte[] getRawData() {
+		return rawData;
+	}
+
+	public void setRawData(byte[] rawData) {
+		this.rawData = rawData;
+	}
+
+	public Map<String, Object> getData() {
+		return data;
+	}
+
+	public void setData(Map<String, Object> data) {
+		this.data = data;
+	}
+
+	@Override
+	public String toString() {
+		return "Packet [dataLengt=" + dataLengt + ", rawData=" + Arrays.toString(rawData) + ", data=" + data + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + dataLengt;
+		result = prime * result + Arrays.hashCode(rawData);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Packet other = (Packet) obj;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		if (dataLengt != other.dataLengt)
+			return false;
+		if (!Arrays.equals(rawData, other.rawData))
+			return false;
+		return true;
+	}
+
+}
---
+++ openCF.protocol/src/de/openCF/protocol/PacketReader.java
@@ -0,0 +1,52 @@
+package de.openCF.protocol;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+public class PacketReader {
+
+	private DataInputStream	dataInputStream	= null;
+
+	public PacketReader(InputStream inputStream) {
+		super();
+		this.dataInputStream = new DataInputStream(inputStream);
+	}
+
+	public Packet readPacket() throws IOException {
+		Packet packet = new Packet();
+
+		packet.setDataLengt(dataInputStream.readInt());
+		byte[] rawData = new byte[packet.getDataLengt()];
+		dataInputStream.read(rawData, 0, packet.getDataLengt());
+		packet.setRawData(rawData);
+		packet.setData(parseRawData(rawData));
+
+		return packet;
+	}
+
+	protected Map<String, Object> parseRawData(byte[] rawData) throws IOException {
+		Map<String, Object> map = new HashMap<String, Object>();
+		String data = new String(rawData, Charset.forName("utf8"));
+
+		JSONTokener jsonTokener = new JSONTokener(data);
+		try {
+			JSONObject jsonObject = new JSONObject(jsonTokener);
+			for (String id : JSONObject.getNames(jsonObject)) {
+				map.put(id, jsonObject.get(id));
+			}
+		} catch (JSONException e) {
+			throw new IOException(e.getMessage());
+		}
+
+		return map;
+	}
+
+}

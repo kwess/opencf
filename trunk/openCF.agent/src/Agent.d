7cd6d4f4ae93fb65629f30f869e2556b943ae19e
sebastian.vagedes <sebastian.vagedes@d1be82d6-222f-3150-1200-e2c27961b611>
2012-02-14 17:42:31 +0000

    initial commit

---
+++ trunk/openCF.agent/src/Agent.d
@@ -0,0 +1,25 @@
+import std.stdio;
+import std.file;
+import std.conv;
+import std.system;
+
+import Configuration;
+import Connection;
+
+void main(string[] args) {
+	Configuration configuration = new Configuration("agent.cfg");
+	
+	configuration.printConfiguration();
+	
+	
+	
+	string hostname = configuration.get("hostname");
+	ushort port = to!short(configuration.get("port"));
+	Connection connection = new Connection(hostname, port);
+	connection.connect();
+	string agent = configuration.get("agentid");
+	string myversion = configuration.get("version");
+	string plattform = configuration.get("plattform");
+	connection.sendHello(agent, myversion, plattform);
+	connection.disconnect();
+} 

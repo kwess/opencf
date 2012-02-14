0882930d8414cb4fdc2748e73f1751803ac454ba
kristian.wessels@gmail.com <kristian.wessels@gmail.com@d1be82d6-222f-3150-1200-e2c27961b611>
2012-02-14 13:33:37 +0000

    logging

--- openCF.server/src/de/openCF/server/persistence/Persistence.java
+++ openCF.server/src/de/openCF/server/persistence/Persistence.java
@@ -1,5 +1,6 @@
 package de.openCF.server.persistence;
 
+import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -9,17 +10,22 @@ import de.openCF.server.data.Server;
 
 public abstract class Persistence {
 
+	private static Logger				logger			= Logger.getLogger(Persistence.class);
 	public static final Configuration	CONFIGURATION	= new Configuration();
 	protected static SessionFactory		SESSION_FACTORY;
 
 	static {
+		logger.debug("adding " + Server.class);
 		CONFIGURATION.addClass(Server.class);
+		logger.debug("adding " + Agent.class);
 		CONFIGURATION.addClass(Agent.class);
 
+		logger.debug("building session factory");
 		SESSION_FACTORY = CONFIGURATION.buildSessionFactory();
 	}
 
 	public static Session getSession() {
+		logger.trace("getSession");
 		return SESSION_FACTORY.openSession();
 	}
 

18d1ff779d0cc92e0ffb17b01ad5d405ae0437f3
kristian.wessels <kristian.wessels@d1be82d6-222f-3150-1200-e2c27961b611>
2012-02-16 17:34:47 +0000

    umstrukturierung

---
+++ openCF.protocol/src/de/openCF/protocol/DefaultPacketHandler.java
@@ -0,0 +1,17 @@
+package de.openCF.protocol;
+
+public class DefaultPacketHandler implements PacketHandler {
+
+	@Override
+	public void handlePacket(Packet packet) {
+	}
+
+	@Override
+	public void handleClose() {
+	}
+
+	@Override
+	public void handleOpen() {
+	}
+
+}

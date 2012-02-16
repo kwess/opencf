98f7fb45ba25f438a7474c894c20ad9930555a59
kristian.wessels <kristian.wessels@d1be82d6-222f-3150-1200-e2c27961b611>
2012-02-16 16:09:47 +0000

    verbesserte reader struktur

--- openCF.server/src/de/openCF/server/connector/AutomationStatusListener.java
+++
@@ -1,9 +0,0 @@
-package de.openCF.server.connector;
-
-import de.openCF.server.data.AutomationStatus;
-
-public interface AutomationStatusListener {
-
-	public void statusChanged(Integer id, AutomationStatus status);
-
-}

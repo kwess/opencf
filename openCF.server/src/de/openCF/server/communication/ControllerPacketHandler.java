89f4ac52937b62d776d0ae400d48d44e7424fb75
kristian.wessels <kristian.wessels@d1be82d6-222f-3150-1200-e2c27961b611>
2012-02-16 16:08:10 +0000

    verbesserte reader struktur

---
+++ openCF.server/src/de/openCF/server/communication/ControllerPacketHandler.java
@@ -0,0 +1,68 @@
+package de.openCF.server.communication;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import de.openCF.protocol.Packet;
+import de.openCF.protocol.PacketHandler;
+
+public class ControllerPacketHandler implements PacketHandler {
+
+	private static Logger	logger	= Logger.getLogger(ControllerPacketHandler.class);
+
+	@Override
+	public void handlePacket(Packet packet) {
+		logger.trace("handlePacket");
+
+		Map<String, Object> data = packet.getData();
+
+		String key = PacketKeys.TYPE;
+		Integer type = (Integer) data.get(key);
+
+		if (type == null)
+			type = PacketType.INVALID;
+
+		switch (type) {
+			case PacketType.AUTOMATION_CONTROL:
+				logger.debug("automation control");
+				handleAutomationControl(data);
+				break;
+			default:
+				logger.warn("unexpected type: " + type);
+				break;
+		}
+
+		logger.trace("handle packet finished");
+	}
+
+	@SuppressWarnings("unchecked")
+	private void handleAutomationControl(Map<String, Object> data) {
+		logger.trace("handleAutomationControl");
+
+		List<String> agent_ids = (List<String>) data.get(PacketKeys.AGENT_ID);
+		String action = (String) data.get(PacketKeys.AUTOMATION_ACTION);
+		String reason = (String) data.get(PacketKeys.AUTOMATION_REASON);
+		String descriptor = (String) data.get(PacketKeys.AUTOMATION_DESCRIPTOR);
+		Map<String, Object> parameter = (Map<String, Object>) data.get(PacketKeys.AUTOMATION_PARAMETER);
+
+		logger.debug("agent_ids: " + agent_ids);
+		logger.debug("action: " + action);
+		logger.debug("reason: " + reason);
+		logger.debug("descriptor: " + descriptor);
+		logger.debug("parameter: " + parameter);
+
+	}
+
+	@Override
+	public void handleClose() {
+		logger.trace("handleClose");
+	}
+
+	@Override
+	public void handleOpen() {
+		logger.trace("handleOpen");
+	}
+
+}
---
+++ openCF.server/src/de/openCF/server/communication/PacketKeys.java
@@ -0,0 +1,22 @@
+package de.openCF.server.communication;
+
+public abstract class PacketKeys {
+
+	public static final String	TYPE					= "type";
+	public static final String	AGENT_ID				= "agent_id";
+	public static final String	AGENT_VERSION			= "agent_version";
+	public static final String	AGENT_PLATTFORM			= "agent_plattform";
+	public static final String	SUCCESSFULL				= "successfull";
+	public static final String	RETURN_CODE				= "return_code";
+	public static final String	MESSAGE					= "message";
+	public static final String	AGENT_LOCAL_TIME		= "local_time";
+	public static final String	REPOSITORY_URL			= "repository_url";
+	public static final String	AUTOMATION_DESCRIPTOR	= "automation_descriptor";
+	public static final String	AUTOMATION_ID			= "automation_id";
+	public static final String	AUTOMATION_ACTION		= "automation_action";
+	public static final String	AUTOMATION_STATUS		= "automation_status";
+	public static final String	AUTOMATION_MESSAGE		= "automation_message";
+	public static final String	AUTOMATION_REASON		= "automation_reason";
+	public static final String	AUTOMATION_PARAMETER	= "automation_parameter";
+
+}

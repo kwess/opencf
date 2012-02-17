ecc908a475468c40032ac85582f346fca470ed67
sebastian.vagedes <sebastian.vagedes@d1be82d6-222f-3150-1200-e2c27961b611>
2012-02-17 15:27:50 +0000

    heartbeat funktioniert jetzt nach hello + helloOK

--- trunk/openCF.agent/src/SocketWriter.d
+++ trunk/openCF.agent/src/SocketWriter.d
@@ -17,48 +17,27 @@ class SocketWriter : Thread {
 		super(&run);
 		this.stream = stream;
 		this.connectionTid = tid;
+		stdout.writeln("SocketWriter ready to send data");
 	}
 	
 	private void run() {
-		stdout.writeln("SocketWriter ready to send data");
-		std.concurrency.send(connectionTid, true);
-		std.concurrency.receive(
-			(bool go){
-				stdout.writeln("socketwriter go: ", go);
-				if(go == false) {
-					return;
-				}
-			}
-		);
+		stdout.writeln("SocketWriter starting to heartbeat");
 		while(this.isRunning) {
-			stdout.writeln("running");
-			//TODO hier warten über events
-//			Thread.sleep(dur!("seconds")(5));
-//			JSONValue json;
-//			json.type = JSON_TYPE.OBJECT;
-//			json.object[type] = JSONValue();
-//			json.object[type].type = JSON_TYPE.INTEGER;
-//			json.object[type].integer = type_heartbeat;
-//			json.object[local_time] = JSONValue();
-//			json.object[local_time].type = JSON_TYPE.STRING;
-//			json.object[local_time].str = text(core.stdc.time.time(null));
-//			Packet p = new Packet(json);
-//			send(p);
+			JSONValue json;
+			json.type = JSON_TYPE.OBJECT;
+			json.object[type] = JSONValue();
+			json.object[type].type = JSON_TYPE.INTEGER;
+			json.object[type].integer = type_heartbeat;
+			json.object[local_time] = JSONValue();
+			json.object[local_time].type = JSON_TYPE.STRING;
+			json.object[local_time].str = text(core.stdc.time.time(null));
+			Packet p = new Packet(json);
+			send(p);
 			
 			Thread.sleep(dur!("seconds")(1));
 		}
 		stdout.writeln("writer ending");
 	}
-	
-	
-//	
-//	public bool send(string message) {
-//		stdout.writefln("sending %s", message);
-//		this.stream.write(bswap(message.length));
-//		this.stream.writeString(message);
-//		
-//		return true;
-//	}
 	
 	public bool send(Packet p) {
 		string text = p.toString();

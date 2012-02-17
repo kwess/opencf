1dd7241daf5cf7383b715dd99be9d721cc24c19c
sebastian.vagedes <sebastian.vagedes@d1be82d6-222f-3150-1200-e2c27961b611>
2012-02-17 11:07:10 +0000

    1. versuch, xml einzusetzten. sendhello wird schon damit gemacht

--- trunk/openCF.agent/src/Agent.d
+++ trunk/openCF.agent/src/Agent.d
@@ -6,6 +6,9 @@ import std.process;
 import core.thread;
 import std.concurrency;
 import std.stream;
+import std.xml;
+import std.array;
+import std.string;
 
 import Configuration;
 import Connection;
@@ -13,6 +16,30 @@ import AutomationThreadManager;
 import AutomationThread;
 
 void main() {
+	
+	
+
+//	auto s = "<Test>What &amp; Up</Test>";
+//	check(s);
+//	auto xml = new DocumentParser(s);
+//
+//	xml.onEndTag["Test"] = (in Element e) {
+//		writeln("Elem: ", e.text);
+//	};
+//	xml.parse();
+//	
+//	writeln(xml);
+//	
+//	Element[] elements;
+//	elements.insertInPlace(0, new Element("type","1"));
+//	elements.insertInPlace(0, new Element("message","2"));
+//	string s2;
+//	foreach(e; elements) {
+//		s2 ~= e.toString();
+//	}
+//	auto doc = new Document(s2);
+//	writeln(doc);
+	    
 	/* read configuration from agent.cfg */
 	Configuration configuration = new Configuration("agent.cfg");
 	configuration.printConfiguration();
--- trunk/openCF.agent/src/SocketListener.d
+++ trunk/openCF.agent/src/SocketListener.d
@@ -26,14 +26,24 @@ class SocketListener : Thread {
 			string datastring = cast(string) stream.readString(bswap(n));
 			stdout.writeln(datastring, " gelesen");
 			Packet p = new Packet(datastring);
+			stdout.writeln("p.getType(): ", p.getType());
+			stdout.writeln(p.getType() == type_agenthelloresponse);
 			
-			switch(p.getType) {
+			switch(p.getType()) {
 				case type_agenthelloresponse:
+					handleAgentHelloResponse(connectionTid, p);
 					break;
 				default:
 					
 			}
 		}
 		stdout.writeln("stop listening");
+	}
+	
+	private void handleAgentHelloResponse(Tid connectionTid, Packet p) {
+		stdout.writeln("handle function", p.getJson.object[successfull]);
+//		//send(connectionTid, p.getJson.object[successfull]);
+//		bool success = p.getJson.object[successfull];
+//		send(connectionTid, true);
 	}
 }

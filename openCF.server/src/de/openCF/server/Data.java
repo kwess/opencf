f152f41913ac4975ee6f3a69c06e868a0d779e9e
kristian.wessels@gmail.com <kristian.wessels@gmail.com@d1be82d6-222f-3150-1200-e2c27961b611>
2012-03-25 17:09:45 +0000

    doku

--- openCF.server/src/de/openCF/server/Data.java
+++ openCF.server/src/de/openCF/server/Data.java
@@ -11,6 +11,14 @@ import de.openCF.protocol.Connection;
 import de.openCF.server.communication.AutomationStatusListener;
 import de.openCF.server.data.AutomationStatus;
 
+/**
+ * 
+ * Data Class for keeping runtime Data like assignment of an agent identifier to
+ * its connection reference.
+ * 
+ * @author kristian.wessels
+ * 
+ */
 public class Data {
 
 	private static Logger										logger						= Logger.getLogger(Data.class);
@@ -18,41 +26,94 @@ public class Data {
 	private static Map<String, Connection>						connections					= new HashMap<String, Connection>();
 	private static Map<Integer, List<AutomationStatusListener>>	automationStatusListener	= new HashMap<Integer, List<AutomationStatusListener>>();
 
+	/**
+	 * Stores a agend identifier - connection association
+	 * 
+	 * @param key
+	 *            agent identifier
+	 * @param connection
+	 *            reference to its corresponding connection instance
+	 * @return the connection which has been stored
+	 */
 	public synchronized static Connection addConnection(String key, Connection connection) {
 		logger.trace("addConnection(String, Connection)");
 		return connections.put(key, connection);
 	}
 
+	/**
+	 * Retrieves a connection identified by the agent id
+	 * 
+	 * @param key
+	 *            agent identifier
+	 * @return the associated connection
+	 */
 	public synchronized static Connection getConnection(String key) {
 		logger.trace("getConnection(String)");
 		return connections.get(key);
 	}
 
+	/**
+	 * Removes the connection
+	 * 
+	 * @param key
+	 *            agent id
+	 * @return the removed connection
+	 */
 	public synchronized static Connection removeConnection(String key) {
 		logger.trace("removeConnection(String)");
 		return connections.remove(key);
 	}
 
+	/**
+	 * Sets the actual Server identifier
+	 * 
+	 * @param server
+	 *            Server identifier
+	 */
 	public synchronized static void setServer(String server) {
 		logger.trace("setServer(Server)");
 		Data.server_id = server;
 	}
 
+	/**
+	 * Get the actual Server identifier
+	 * 
+	 * @return Server idenitifer
+	 */
 	public synchronized static String getServer() {
 		logger.trace("getServer");
 		return Data.server_id;
 	}
 
+	/**
+	 * Check the status of the agent whether its online
+	 * 
+	 * @param aid
+	 *            agent identifier
+	 * @return online-status
+	 */
 	public synchronized static boolean isAgenOnline(String aid) {
 		return connections.containsKey(aid);
 	}
 
+	/**
+	 * Removes an Listener of an automation Status
+	 * 
+	 * @param listener
+	 */
 	public synchronized static void removeAutomationStatusListener(AutomationStatusListener listener) {
 		for (Map.Entry<Integer, List<AutomationStatusListener>> e : automationStatusListener.entrySet()) {
 			e.getValue().remove(listener);
 		}
 	}
 
+	/**
+	 * 
+	 * 
+	 * @param aid
+	 * @param listener
+	 * @return
+	 */
 	public synchronized static boolean addAutomationStatusListerner(Integer aid, AutomationStatusListener listener) {
 		logger.trace("addAutomationStatusListener(Integer, AutomationStatusListener)");
 		List<AutomationStatusListener> list = Data.automationStatusListener.get(aid);

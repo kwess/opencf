bb647ddebb00b6c745e9f1f5e415ae86d259a25a
kristian.wessels <kristian.wessels@d1be82d6-222f-3150-1200-e2c27961b611>
2012-10-07 10:38:31 +0000

    Server Configuration refactored

--- trunk/openCF.server/src/de/openCF/server/Server.java
+++ trunk/openCF.server/src/de/openCF/server/Server.java
@@ -1,20 +1,14 @@
 package de.openCF.server;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.nio.ByteOrder;
 import java.util.Date;
-import java.util.Properties;
 import java.util.concurrent.Executors;
 
 import org.apache.log4j.Logger;
 
 import de.openCF.protocol.Acceptor;
-import de.openCF.protocol.PacketHelper.Encoding;
 import de.openCF.server.communication.AgentConnectionFactory;
 import de.openCF.server.communication.ControllerConnectionFactory;
 import de.openCF.server.data.Plattform;
@@ -23,68 +17,24 @@ import de.openCF.server.persistence.Persistence;
 
 public class Server implements Runnable {
 
-	public static final String	CONFIG_FILE					= "config/server.properties";
+	public static final String	CONFIG_FILE		= "config/server.properties";
 
-	private static final String	SERVER_ID					= "openCF.server.name";
-
-	private static final String	AGENT_PORT					= "openCF.server.acceptor.agent.port";
-	private static final String	AGENT_NEEDSCLIENTAUTH		= "openCF.server.acceptor.agent.needsClientAuth";
-	private static final String	AGENT_POOLSIZE				= "openCF.server.acceptor.agent.poolSize";
-	private static final String	AGENT_USE_SSL				= "openCF.server.acceptor.agent.useSSL";
-	private static final String	AGENT_DEBUG					= "openCF.server.acceptor.agent.debug";
-	private static final String	AGENT_PROTOCOL_ENCODING		= "openCF.server.acceptor.agent.protocol.encoding";
-
-	private static final String	CONTROLLER_PORT				= "openCF.server.acceptor.controller.port";
-	private static final String	CONTROLLER_NEEDSCLIENTAUTH	= "openCF.server.acceptor.controller.needsClientAuth";
-	private static final String	CONTROLLER_POOLSIZE			= "openCF.server.acceptor.controller.poolSize";
-	private static final String	CONTROLLER_USE_SSL			= "openCF.server.acceptor.controller.useSSL";
-	private static final String	CONTROLLER_DEBUG			= "openCF.server.acceptor.controller.debug";
-
-	private Logger				logger						= Logger.getLogger(Server.class);
-	private Properties			properties					= new Properties();
+	private Logger				logger			= Logger.getLogger(Server.class);
+	private ServerConfiguration	configuration	= new ServerConfiguration();
 
 	public Server() {
-		logger.trace("new");
 
-		logger.debug("open config_file: " + CONFIG_FILE);
-		File file = new File(CONFIG_FILE);
-		FileInputStream fileInputStream;
-		try {
-			logger.trace("reading from config_file");
-			fileInputStream = new FileInputStream(file);
-			logger.trace("loading config_file into properties");
-			this.properties.load(fileInputStream);
-		} catch (FileNotFoundException e) {
-			logger.error("config file not found", e);
-		} catch (IOException e) {
-			logger.error("cant read config file", e);
-		}
 	}
 
 	@Override
 	public void run() {
-		logger.trace("run");
+		configuration.load(CONFIG_FILE);
 
 		Persistence persistence = Persistence.getInstance();
 
 		ByteOrder byteOrder = ByteOrder.nativeOrder();
 
 		logger.debug("native byte order: " + byteOrder);
-
-		logger.info("loading settings");
-
-		Integer agentPort = Integer.parseInt(this.properties.getProperty(AGENT_PORT, "12345"));
-		Boolean agentNeedsClientAuth = Boolean.parseBoolean(this.properties.getProperty(AGENT_NEEDSCLIENTAUTH, "false"));
-		Integer agentConnectionPoolSize = Integer.parseInt(this.properties.getProperty(AGENT_POOLSIZE, "1024"));
-		Boolean agentUseSSL = Boolean.parseBoolean(this.properties.getProperty(AGENT_USE_SSL, "false"));
-		Boolean agentDebug = Boolean.parseBoolean(this.properties.getProperty(AGENT_DEBUG, "false"));
-		Encoding agentEncoding = Encoding.valueOf(this.properties.getProperty(AGENT_PROTOCOL_ENCODING));
-
-		Integer controllerPort = Integer.parseInt(this.properties.getProperty(CONTROLLER_PORT, "12346"));
-		Boolean controllerNeedsClientAuth = Boolean.parseBoolean(this.properties.getProperty(CONTROLLER_NEEDSCLIENTAUTH, "false"));
-		Integer controllerConnectionPoolSize = Integer.parseInt(this.properties.getProperty(CONTROLLER_POOLSIZE, "1024"));
-		Boolean controllerUseSSL = Boolean.parseBoolean(this.properties.getProperty(CONTROLLER_USE_SSL, "false"));
-		Boolean controllerDebug = Boolean.parseBoolean(this.properties.getProperty(CONTROLLER_DEBUG, "false"));
 
 		String hostname = "unknown";
 		try {
@@ -94,7 +44,7 @@ public class Server implements Runnable {
 			logger.warn("cant get hostname of localhost", e);
 		}
 
-		String server_id = this.properties.getProperty(SERVER_ID);
+		String server_id = configuration.getServerId();
 		de.openCF.server.data.Server server = (de.openCF.server.data.Server) persistence.get(de.openCF.server.data.Server.class, server_id);
 		if (server != null) {
 			logger.info("server loaded");
@@ -102,8 +52,8 @@ public class Server implements Runnable {
 			logger.info("server online for first time");
 			server = new de.openCF.server.data.Server(server_id);
 		}
-		server.setAgentPort(agentPort);
-		server.setControllerPort(controllerPort);
+		server.setAgentPort(configuration.getAgentPort());
+		server.setControllerPort(configuration.getControllerPort());
 		server.setPlattform(Plattform.GENERIC);
 		server.setStatus(Status.ONLINE);
 		server.setUpdated(new Date());
@@ -118,20 +68,20 @@ public class Server implements Runnable {
 		logger.debug("createing acceptor for agents");
 
 		Acceptor acceptorAgents = new Acceptor();
-		acceptorAgents.setPort(agentPort);
-		acceptorAgents.setNeedsClientAuth(agentNeedsClientAuth);
-		acceptorAgents.setConnectionPoolSize(agentConnectionPoolSize);
-		acceptorAgents.setUseSSL(agentUseSSL);
-		acceptorAgents.setConnectionFactory(new AgentConnectionFactory(agentDebug, agentEncoding));
+		acceptorAgents.setPort(configuration.getAgentPort());
+		acceptorAgents.setNeedsClientAuth(configuration.getAgentNeedsClientAuth());
+		acceptorAgents.setConnectionPoolSize(configuration.getAgentConnectionPoolSize());
+		acceptorAgents.setUseSSL(configuration.getAgentUseSSL());
+		acceptorAgents.setConnectionFactory(new AgentConnectionFactory(configuration.getAgentDebug(), configuration.getAgentEncoding()));
 
 		logger.debug("createing acceptor for controllers");
 
 		Acceptor acceptorControllers = new Acceptor();
-		acceptorControllers.setPort(controllerPort);
-		acceptorControllers.setNeedsClientAuth(controllerNeedsClientAuth);
-		acceptorControllers.setConnectionPoolSize(controllerConnectionPoolSize);
-		acceptorControllers.setUseSSL(controllerUseSSL);
-		acceptorControllers.setConnectionFactory(new ControllerConnectionFactory(controllerDebug));
+		acceptorControllers.setPort(configuration.getControllerPort());
+		acceptorControllers.setNeedsClientAuth(configuration.getControllerNeedsClientAuth());
+		acceptorControllers.setConnectionPoolSize(configuration.getControllerConnectionPoolSize());
+		acceptorControllers.setUseSSL(configuration.getControllerUseSSL());
+		acceptorControllers.setConnectionFactory(new ControllerConnectionFactory(configuration.getControllerDebug()));
 
 		logger.info("server online");
 		logger.debug(server);
